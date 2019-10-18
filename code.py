19bcf7ada9bc7e313a97023fff124a9f04ecc910
prateekprabhu <56713081+prateekprabhu@users.noreply.github.com>
2019-10-18 20:16:49 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,24 +3,31 @@ import numpy as np
 import cv2
 
 
-inp_img = cv2.imread('cameraman.tif', 0); #input image/frames
+inp_img = cv2.imread('cameraman.tif') #input image/frames
+#print(inp_img);
+#inp_img = np.array(inp_img)
+cv2.imshow("inp_img", inp_img)
 
 def darkGamma(inp_img): #gamma correction for dark image - light correction
-    g = 1,4;
+    g = 1.4;
     out_img = np.power(inp_img, g);
-    cv2.imshow(out_img);
+    #print(out_img);
+    out_img = out_img.astype(np.uint8);
+    cv2.imshow('',out_img);
     #return out_img;
     
     
 def lightGamma(inp_img): #gamma correction for light image - light correction
     g = 0.4;
     out_img = np.power(inp_img, g);
-    cv2.imshow(out_img);
+    out_img = out_img.astype(np.uint8);
+    cv2.imshow('',out_img);
     #return out_img;
     
 def histogramEQ(inp_img): #histogram equaliation - contrast correction
     out_img = cv2.equalizeHist(inp_img);
-    cv2.imshow(out_img);
+    out_img = out_img.astype(np.uint8);
+    cv2.imshow('',out_img);
     #return out_img;
     
 def unsharpMask(inp_img): #unsharp masking - Image sharpening
@@ -30,11 +37,15 @@ def unsharpMask(inp_img): #unsharp masking - Image sharpening
     mask = cv2.addWeighted(inp_img, 1, conv_img, -1, 0);
     
     out_img = cv2.addWeighted(inp_img, 1, mask, 1, 0);
-    cv2.imshow(out_img);
+    out_img = out_img.astype(np.uint8);
+    out_img = out_img.astype(np.uint8);
+    cv2.imshow('',out_img);
     #return out_img;
     
 def saltPepNoise(inp_img): #to denoise salt and pepper
     out_img = cv2.medianBlur(inp_img, 3);
-    cv2.imshow(out_img);
+    out_img = out_img.astype(np.uint8);
+    cv2.imshow(out_img, '');
     #return out_img;
     
+#unsharpMask(inp_img)
