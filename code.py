161ebcc457e4438024af7b3ad6ca388d6ac93e02
prateekprabhu <56713081+prateekprabhu@users.noreply.github.com>
2019-10-19 22:23:11 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,51 +1,53 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
+"""
+Created on Fri Oct 18 23:10:58 2019
+
+@author: prateek_prabhu
+"""
+
 import cv2
+import numpy as np
+in_img = cv2.imread('b.png')
+cv2.imshow("in_img", in_img)
 
 
-inp_img = cv2.imread('cameraman.tif') #input image/frames
-#print(inp_img);
-#inp_img = np.array(inp_img)
-cv2.imshow("inp_img", inp_img)
+def brighten(img, val):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+#    print(v)
+    lim = 255 - val
 
-def darkGamma(inp_img): #gamma correction for dark image - light correction
-    g = 1.4;
-    out_img = np.power(inp_img, g);
-    #print(out_img);
-    out_img = out_img.astype(np.uint8);
-    cv2.imshow('',out_img);
-    #return out_img;
-    
-    
-def lightGamma(inp_img): #gamma correction for light image - light correction
-    g = 0.4;
-    out_img = np.power(inp_img, g);
-    out_img = out_img.astype(np.uint8);
-    cv2.imshow('',out_img);
-    #return out_img;
-    
-def histogramEQ(inp_img): #histogram equaliation - contrast correction
-    out_img = cv2.equalizeHist(inp_img);
-    out_img = out_img.astype(np.uint8);
-    cv2.imshow('',out_img);
-    #return out_img;
-    
-def unsharpMask(inp_img): #unsharp masking - Image sharpening
-    kernel = np.ones((5,5), np.float32)/25;
-    conv_img = cv2.filter2D(inp_img, -1, kernel);
-    
-    mask = cv2.addWeighted(inp_img, 1, conv_img, -1, 0);
-    
-    out_img = cv2.addWeighted(inp_img, 1, mask, 1, 0);
-    out_img = out_img.astype(np.uint8);
-    out_img = out_img.astype(np.uint8);
-    cv2.imshow('',out_img);
-    #return out_img;
-    
-def saltPepNoise(inp_img): #to denoise salt and pepper
-    out_img = cv2.medianBlur(inp_img, 3);
-    out_img = out_img.astype(np.uint8);
-    cv2.imshow(out_img, '');
-    #return out_img;
-    
-#unsharpMask(inp_img)
+    v[v > lim] = 255
+    v[v <= lim] += val
+
+    final_hsv = cv2.merge((h,s,v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
+
+def unsharpmask(inp_img):  # unsharp masking - Image sharpening
+    kernel = np.ones((5, 5), np.float32) / 25
+    conv_img = cv2.filter2D(inp_img, -1, kernel)
+
+    mask = cv2.addWeighted(inp_img, 1, conv_img, -1, 0)
+
+    out_img = cv2.addWeighted(inp_img, 1, mask, 1, 0)
+    out_img = out_img.astype(np.uint8)
+    out_img = out_img.astype(np.uint8)
+    return out_img;
+
+
+def darken(image, gamma):
+    invG = 1.0/gamma
+    table = np.array([((i / 255.0) ** invG) * 255 for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, table)
+
+
+out1 = brighten(in_img, 50)
+out2 = darken(in_img, 0.5)
+out3 = unsharpmask(out1)
+#cv2.imshow("out_img1", out1)
+cv2.imshow("out_img2", out2)
+cv2.imshow("mask", out3)
+cv2.waitKey()
